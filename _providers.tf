66cae30ceb1972fd112dfd185d8fdb35f36214e3
Todd C <oddbodtodd@gmail.com>
2026-03-12 16:42:37 +1000

    terraform init

--- _providers.tf
+++ _providers.tf
@@ -2,14 +2,16 @@
 ## Providers
 ##
 terraform {
-  required_version = "~> 1.14.1"
-  }
+  required_version = "~> 1.11.4"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.100.0"
     }
   }
+}
+
 
 
 provider "aws" {
